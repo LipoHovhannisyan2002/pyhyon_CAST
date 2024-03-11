dec74f183f8f4c455fabe9e42731e73f68253b99
LipoHovhannisyan2002 <51203233+LipoHovhannisyan2002@users.noreply.github.com>
2024-03-11 12:24:15 +0400

    Update Number_of_weak_char.py

--- Number_of_weak_char.py
+++ Number_of_weak_char.py
@@ -1,22 +1,21 @@
 def numberOfWeakCharacters(properties):
     weak_count = 0
-    for  i in range(len(properties)):
+    for i in range(len(properties)):
         is_weak = False
-        current_attack,current_defense = properties[i]
+        current_attack, current_defense = properties[i]
         for j in range(len(properties)):
-            if i!=j:
-                their_attack, other_defense = properties[j]
-                if their_attack > current_attack and other_defense > current_defense:
-                    weak_count +=1
-                    break
 
+            their_attack, other_defense = properties[j]
+            if their_attack > current_attack and other_defense > current_defense:
+                weak_count += 1
+                break
 
     return weak_count
 
 
-properties = [[5,5],[6,3],[3,6]]
+properties = [[5, 5], [6, 3], [3, 6]]
 print(numberOfWeakCharacters(properties))
-properties = [[2,2],[3,3]]
+properties = [[2, 2], [3, 3]]
 print(numberOfWeakCharacters(properties))
-properties = [[1,5],[10,4],[4,3]]
+properties = [[1, 5], [10, 4], [4, 3]]
 print(numberOfWeakCharacters(properties))
