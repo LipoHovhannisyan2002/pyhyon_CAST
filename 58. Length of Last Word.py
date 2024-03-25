5cfff8714ddb9c6635f94c0c4b0c63518350af0d
LipoHovhannisyan2002 <51203233+LipoHovhannisyan2002@users.noreply.github.com>
2024-03-25 16:33:09 +0400

    ISSUE#2. Removed the split() and strip() functions.
    Everything is working correctly.

--- 58. Length of Last Word.py
+++ 58. Length of Last Word.py
@@ -1,30 +1,23 @@
-def split_string(input_string, delimiter):
-    substrings = []
-    start_index = 0
-
-    for i, char in enumerate(input_string):
-        if char == delimiter:
-            substring = input_string[start_index:i]
-            substrings.append(substring)
-            start_index = i + 1
-
-    last_substring = input_string[start_index:]
-    substrings.append(last_substring)
-
-    return substrings
-
 def lengthOfLastWord(input_string):
-    delimiter = " "
-    splited_sent = split_string(input_string.strip(), delimiter)
-    i = len(splited_sent)
-
-    for item in range(i - 1, -1, -1):
-        if splited_sent[item] and (65 <= ord(splited_sent[item][-1]) <= 90) or (
-                97 <= ord(splited_sent[item][-1]) <= 122):
-            return len(splited_sent[item])
-
-    return 0  
+    length = len(input_string)
+    last_word_length = 0
+    last_word_found = False
 
 
-s = "H ello World! This is a sample string "
+    for i in range(length - 1, -1, -1):
+        if input_string[i] != ' ':
+            last_word_length += 1
+            last_word_found = True
+
+
+
+        elif last_word_found:
+            break
+
+
+
+
+    return last_word_length
+
+s = "Hello World This "
 print(lengthOfLastWord(s))
