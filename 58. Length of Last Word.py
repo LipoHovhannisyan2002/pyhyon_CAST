64bbe480472c8c6808efc6ba789044b3d447464c
LipoHovhannisyan2002 <51203233+LipoHovhannisyan2002@users.noreply.github.com>
2024-03-25 15:51:10 +0400

    Update 58. Length of Last Word.py
    Issue #1: The split() function was written by an algorithmic version.

--- 58. Length of Last Word.py
+++ 58. Length of Last Word.py
@@ -1,21 +1,30 @@
-s = "Hello world   "
+def split_string(input_string, delimiter):
+    substrings = []
+    start_index = 0
 
+    for i, char in enumerate(input_string):
+        if char == delimiter:
+            substring = input_string[start_index:i]
+            substrings.append(substring)
+            start_index = i + 1
 
-def lengthOfLastWord(a):
-    result = False
-    splited_sent = s.split()
+    last_substring = input_string[start_index:]
+    substrings.append(last_substring)
+
+    return substrings
+
+def lengthOfLastWord(input_string):
+    delimiter = " "
+    splited_sent = split_string(input_string.strip(), delimiter)
     i = len(splited_sent)
 
-    for item in range(i-1,-1,-1):
+    for item in range(i - 1, -1, -1):
+        if splited_sent[item] and (65 <= ord(splited_sent[item][-1]) <= 90) or (
+                97 <= ord(splited_sent[item][-1]) <= 122):
+            return len(splited_sent[item])
+
+    return 0  
 
 
-        if splited_sent[item] and (65 <= ord(splited_sent[item][-1]) <= 90) or (97 <= ord(splited_sent[item][-1]) <= 122):
-
-            result = True
-            break
-        x = i -1
-
-    if result:
-        return len(splited_sent[item])
-
+s = "H ello World! This is a sample string "
 print(lengthOfLastWord(s))
